a519c050239afa00c6980ea6735852a19fcd82dc
minamifan <122370462+minamifan@users.noreply.github.com>
2023-01-17 09:41:05 -0500

    added logic

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
+//initial setup
+let a=3;
+let b=5;
+let c;
+
+let output='let a =3;\n'
+output +='let b=5;\n';
+output+='let c;\n';
+output+='------------\n'
+output+='a+b='+(a+b)+"\n"
+alert(output);
+
+let sum =a+b;
+let sub=a-b;
+
 
